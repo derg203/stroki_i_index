e882a0e932b7038c9938d32fafb8197c75a5d539
derg203 <ddergacev714@gmail.com>
2024-10-14 13:22:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-a='экстриситет'
+a=input()  #экстриситет
+
 print(a[0])
 print(a[-1])
-print(a[4:])
+print(len(a))
+dlina= len(a)
+polovina = len(a)//2
+d= (dlina-polovina)
+if d%2==0:print(a[polovina+1:])
+else: print(a[polovina:])
 print(a[::-1])
-print(a[0::2])
+print(a[1::2])
